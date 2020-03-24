1254c6ad2ed21693920bea240e66ef7179fdb859
Loop421 <lopez.e421@gmail.com>
2020-03-24 12:24:12 -0400

    Backend

---
+++ fitness/src/main/java/com/ex/controller/PersonController.java
@@ -0,0 +1,42 @@
+package com.ex.controller;
+
+import com.ex.model.Person;
+import com.ex.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping("/users")
+    public List<Person> getAllUser() {
+        return personService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<Person> getByUserId(@PathVariable("id") String id){
+        return personService.getByUserId(id);
+    }
+
+    @PostMapping("/users")
+    public void addUser(@RequestBody Person person){
+        personService.addUser(person);
+    }
+
+    @PutMapping("/users/{id}")
+    public void updateUser(@RequestBody Person person, @PathVariable("id") String id) {
+        personService.updateUser(person, id);
+    }
+
+    @DeleteMapping("users/{id}")
+    public void deleteUser(@PathVariable("id") String id) {
+
+        personService.deleteUser(id);
+    }
+}
